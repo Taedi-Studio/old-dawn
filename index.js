97623691dd4b44c7d687214d977f4aad7ec3daa6
Aletive <seo16hyun@gmail.com>
2020-01-25 02:27:06 +0900

    스승님 도와주세요

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ dawn.on('ready', () => {
   console.log('Dawn bot is online!')
   console.log('Invite Link: https://discordapp.com/api/oauth2/authorize?client_id=' + settings.client_id + '&permissions=0&scope=bot')
 })
+
+dawn.on('message', (msg) => {   
+  if (msg.content === 'ping') {
+    msg.channel.send('pong')
+  }
+})
+
+/* 스승님 이 부분 도와주세요.
+  dawn.setInterval((arg) => {
+    message.channel.send(arg)
+  },10000, "Hello, world!")
+*/
